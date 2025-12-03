37f4746182cf82129c8e323c8391984948ab9602
ea234 <anyang@gmx.de>
2025-12-03 11:27:23 +0100

    Day 2 - Part 2

--- src/de/ea234/day2/Day2GiftShop.java
+++ src/de/ea234/day2/Day2GiftShop.java
@@ -50,16 +50,17 @@ public class Day2GiftShop
    * Your job is to find all of the invalid IDs that appear in the given ranges. In
    * the above example:
    * 
-   *     11-22 has two invalid IDs, 11 and 22.
-   *     95-115 has one invalid ID, 99.
-   *     998-1012 has one invalid ID, 1010.
-   *     1188511880-1188511890 has one invalid ID, 1188511885.
-   *     222220-222224 has one invalid ID, 222222.
-   *     1698522-1698528 contains no invalid IDs.
-   *     446443-446449 has one invalid ID, 446446.
-   *     38593856-38593862 has one invalid ID, 38593859.
+   *     11-22                    has two invalid IDs, 11 and 22.
+   *     95-115                   has one invalid ID, 99.
+   *     998-1012                 has one invalid ID, 1010.
+   *     1188511880-1188511890    has one invalid ID, 1188511885.
+   *     222220-222224            has one invalid ID, 222222.
+   *     1698522-1698528          contains no invalid IDs.
+   *     446443-446449            has one invalid ID, 446446.
+   *     38593856-38593862        has one invalid ID, 38593859.
    * 
    *     The rest of the ranges contain no invalid IDs.
+   *  
    * Adding up all the invalid IDs in this example produces 1227775554.
    * 
    * What do you get if you add up all of the invalid IDs?
@@ -76,21 +77,26 @@ public class Day2GiftShop
    * (12 five times), and 1111111 (1 seven times) are all invalid IDs.
    * 
    * From the same example as before:
-   *     11-22 still has two invalid IDs, 11 and 22.
-   *     95-115 now has two invalid IDs, 99 and 111.
-   *     998-1012 now has two invalid IDs, 999 and 1010.
+   *     11-22                 still has two invalid IDs, 11 and 22.
+   *     95-115                now has two invalid IDs, 99 and 111.
+   *     998-1012              now has two invalid IDs, 999 and 1010.
    *     1188511880-1188511890 still has one invalid ID, 1188511885.
-   *     222220-222224 still has one invalid ID, 222222.
-   *     1698522-1698528 still contains no invalid IDs.
-   *     446443-446449 still has one invalid ID, 446446.
-   *     38593856-38593862 still has one invalid ID, 38593859.
-   *     565653-565659 now has one invalid ID, 565656.
-   *     824824821-824824827 now has one invalid ID, 824824824.
+   *     222220-222224         still has one invalid ID, 222222.
+   *     
+   *     1698522-1698528       still contains no invalid IDs.
+   *     
+   *     446443-446449         still has one invalid ID, 446446.
+   *     38593856-38593862     still has one invalid ID, 38593859.
+   *     565653-565659         now has one invalid ID, 565656.
+   *     824824821-824824827   now has one invalid ID, 824824824.
    *     2121212118-2121212124 now has one invalid ID, 2121212121.
    *     
    * Adding up all the invalid IDs in this example produces 4174379265.
-   * 
+   *                                                        4174379265
+   *                                                        
    * What do you get if you add up all of the invalid IDs using these new rules?
+   * 
+   * sum_ranges 85513235135
    * 
    * 
    * ------------------------------------------------------------------------
@@ -128,20 +134,63 @@ public class Day2GiftShop
    * sum_ranges 1227775554
    * 
    * 
+   * ------------------------------------------------------------------------
+   * 
+   * Range From 11 To 22 = Number of Values 11
+   *     Nr. 1  1 11 is_even  pattern_count 1
+   *     Nr. 12  2 22 is_even  pattern_count 1
+   * 
+   * Range From 95 To 115 = Number of Values 20
+   *     Nr. 5  1 99 is_even  pattern_count 1
+   *     Nr. 17  2 111 is_even  pattern_count 2
+   * 
+   * Range From 998 To 1012 = Number of Values 14
+   *     Nr. 2  1 999 is_even  pattern_count 2
+   *     Nr. 13  2 1010 is_even  pattern_count 1
+   * 
+   * Range From 1188511880 To 1188511890 = Number of Values 10
+   *     Nr. 6  1 1188511885 is_even  pattern_count 1
+   * 
+   * Range From 222220 To 222224 = Number of Values 4
+   *     Nr. 3  1 222222 is_even  pattern_count 1
+   * 
+   * Range From 1698522 To 1698528 = Number of Values 6
+   * 
+   * Range From 446443 To 446449 = Number of Values 6
+   *     Nr. 4  1 446446 is_even  pattern_count 1
+   * 
+   * Range From 38593856 To 38593862 = Number of Values 6
+   *     Nr. 4  1 38593859 is_even  pattern_count 1
+   * 
+   * Range From 565653 To 565659 = Number of Values 6
+   *     Nr. 4  1 565656 is_even  pattern_count 2
+   * 
+   * Range From 824824821 To 824824827 = Number of Values 6
+   *     Nr. 4  1 824824824 is_even  pattern_count 2
+   * 
+   * Range From 2121212118 To 2121212124 = Number of Values 6
+   *     Nr. 4  1 2121212121 is_even  pattern_count 4
+   * 
+   * sum_ranges 4174379265
+   * 
    */
 
   public static void main( String[] args )
   {
-    checkX( getListProd() );
-
     String test_content = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
     List< String > test_content_list = Arrays.stream( test_content.split( "," ) ).map( String::trim ).collect( Collectors.toList() );
 
-    checkX( test_content_list );
+    //checkListPart1( test_content_list );
+    checkListPart2( test_content_list );
+
+    //checkListPart1( getListProd() );
+    checkListPart2( getListProd() );
+
+    //startTestCountNr();
   }
 
-  public static void checkX( List< String > pListIdRanges )
+  public static void checkListPart1( List< String > pListIdRanges )
   {
     if ( pListIdRanges == null )
     {
@@ -156,7 +205,7 @@ public class Day2GiftShop
     {
       String range_string = pListIdRanges.get( rotation_list_index );
 
-      sum_ranges = sum_ranges.add( checkRange( range_string ) );
+      sum_ranges = sum_ranges.add( checkRangePart1( range_string ) );
     }
 
     wl( "" );
@@ -166,7 +215,7 @@ public class Day2GiftShop
     wl( "" );
   }
 
-  private static BigDecimal checkRange( String pRange )
+  private static BigDecimal checkRangePart1( String pRange )
   {
     String[] range_split = pRange.trim().split( "-" );
 
@@ -210,10 +259,6 @@ public class Day2GiftShop
 
         int range_value_str_mitte = range_value_str_len / 2;
 
-        //int str_start_index_1 = 0;
-        //int str_start_index_2 = range_value_str_mitte;
-        //range_value_str.charAt( str_start_index_1 ) == range_value_str.charAt( str_start_index_1 );
-
         String first_half = range_value_str.substring( 0, range_value_str_mitte );
         String second_half = range_value_str.substring( range_value_str_mitte );
 
@@ -231,6 +276,185 @@ public class Day2GiftShop
     }
 
     return sum_counter;
+  }
+
+  public static void checkListPart2( List< String > pListIdRanges )
+  {
+    if ( pListIdRanges == null )
+    {
+      wl( "pListIdRanges == null" );
+
+      return;
+    }
+
+    BigDecimal sum_ranges = new BigDecimal( 0 );
+
+    for ( int rotation_list_index = 0; rotation_list_index < pListIdRanges.size(); rotation_list_index++ )
+    {
+      String range_string = pListIdRanges.get( rotation_list_index );
+
+      sum_ranges = sum_ranges.add( checkRangePart2( range_string ) );
+    }
+
+    wl( "" );
+    wl( "sum_ranges " + sum_ranges.toPlainString() );
+    wl( "" );
+    wl( "" );
+    wl( "" );
+  }
+
+  private static BigDecimal checkRangePart2( String pRange )
+  {
+    String[] range_split = pRange.trim().split( "-" );
+
+    String range_von = range_split[ 0 ];
+    String range_bis = range_split[ 1 ];
+
+    long range_start = Long.parseLong( range_von );
+    long range_end = Long.parseLong( range_bis );
+
+    if ( range_start > range_end )
+    {
+      long temp = range_end;
+
+      range_end = range_start;
+
+      range_start = temp;
+    }
+
+    wl( "" );
+    wl( "Range From " + range_start + " To " + range_end + " = Number of Values " + ( range_end - range_start ) );
+
+    BigDecimal sum_counter = new BigDecimal( 0 );
+
+    int nr_counter = 0;
+
+    int counter_equal = 0;
+
+    for ( long range_value = range_start; range_value <= range_end; range_value++ )
+    {
+      nr_counter++;
+
+      String range_value_str = Long.toString( Math.abs( range_value ) );
+
+      int range_value_str_len = range_value_str.length();
+
+      int range_value_str_mitte = (int) range_value_str_len / 2;
+
+      int index_end_muster = range_value_str_mitte;
+
+      /*
+       * pattern_count does not include the first occurance of the pattern 
+       */
+      int pattern_count = 0;
+
+      while ( ( index_end_muster >= 0 ) && ( pattern_count == 0 ) )
+      {
+        pattern_count = countPattenOccurence( range_value_str, index_end_muster );
+
+        if ( pattern_count == 0 )
+        {
+          index_end_muster--;
+        }
+      }
+
+      boolean knz_number_illegal = pattern_count > 0;
+
+      if ( knz_number_illegal )
+      {
+        counter_equal++;
+
+        sum_counter = sum_counter.add( new BigDecimal( range_value ) );
+
+        wl( "    Nr. " + nr_counter + "  " + counter_equal + " " + range_value + " is_even " + " pattern_count " + ( pattern_count ) );
+      }
+    }
+
+    return sum_counter;
+  }
+
+  private static void startTestCountNr()
+  {
+    testCountNr( "585858", 1 );
+    testCountNr( "12341234", 1 );
+    testCountNr( "12341234", 2 );
+    testCountNr( "12341234", 3 );
+    testCountNr( "12341234", 4 );
+    testCountNr( "12341234", 5 );
+
+    testCountNr( "11111111111", 0 );
+    testCountNr( "11111111111", 1 );
+    testCountNr( "11111111111", 2 );
+    testCountNr( "11111111111", 3 );
+    testCountNr( "11111111111", 4 );
+    testCountNr( "11111111111", 5 );
+    testCountNr( "11111111111", 6 );
+    testCountNr( "11111111111", 7 );
+
+    testCountNr( "1231235", 2 );
+
+    testCountNr( "824824824", 2 );
+
+    testCountNr( "2121212121", 1 );
+    testCountNr( "1188511885", 4 );
+
+    testCountNr( "12345", 4 );
+  }
+
+  private static void testCountNr( String pString, int pPatternIndexEnd )
+  {
+    wl( "countNr( \"" + pString + "\", " + pPatternIndexEnd + " ) = " + countPattenOccurence( pString, pPatternIndexEnd ) );
+  }
+
+  private static int countPattenOccurence( String pString, int pPatternIndexEnd )
+  {
+    int pattern_count = 0;
+
+    if ( pString != null )
+    {
+      int str_length = pString.length();
+
+      if ( pPatternIndexEnd < str_length )
+      {
+        int pattern_index_act = 0;
+        int pattern_index_end = pPatternIndexEnd;
+
+        int pattern_check_index = pPatternIndexEnd + 1;
+
+        while ( pattern_check_index < str_length )
+        {
+          char pattern_char = pString.charAt( pattern_index_act );
+          char check_char = pString.charAt( pattern_check_index );
+
+          if ( pattern_char == check_char )
+          {
+            if ( pattern_index_act == pattern_index_end )
+            {
+              pattern_count++;
+
+              pattern_index_act = 0;
+            }
+            else
+            {
+              pattern_index_act++;
+            }
+
+            pattern_check_index++;
+          }
+          else
+          {
+            return 0; // Muster stimmen nicht ueberein
+          }
+        }
+
+        if ( pattern_index_act != 0 )
+        {
+          return 0;
+        }
+      }
+    }
+
+    return pattern_count;
   }
 
   private static List< String > getListProd()
@@ -262,5 +486,4 @@ public class Day2GiftShop
   {
     System.out.println( pString );
   }
-
 }
